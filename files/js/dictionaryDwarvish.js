25c5572b0a205a2b018ecf6b0b9c30c60f355ea7
mimesandsnails <54565602+mimesandsnails@users.noreply.github.com>
2022-07-21 20:37:06 -0700

    Add files via upload

---
+++ files/js/dictionaryDwarvish.js
@@ -0,0 +1,144 @@
+//DICTIONARY
+// "": "",
+
+//OFFICIAL DICTIONARY WORDS ARE IN BLUE
+  const dictionaryDwarvish = {
+    //PRONOUNS
+    "he": "",
+    "his": " (personal adjective) /  (personal pronoun)",
+    "i": "",
+    "it": " (subject pronoun) /  (object pronoun)",
+    "one": "",
+    "that": "mika",
+    "their": "",
+    "they": "",
+    "this": "mina",
+    "us": "",
+    "we": "",
+    "you": " (sp) /  (op) /  / ",
+
+    //ARTICLES
+    "a": "",
+    "an": "",
+    "the": "",
+
+    //CONJUNCTIONS
+    "and": "",
+    "but": "asar",
+    "or": "",
+    "if": "",
+    "also": "",
+
+    //PREPOSITIONS
+    "at": "bok",
+    "back": "",
+    "from": "olen",
+    "in": "",
+    "near": "",
+    "of": "",
+    "on": "aen",
+    "out": "",
+    "to": "",
+    "with": "bathor",
+
+    //ADJECTIVES
+    "big": "",
+    "bold": "",
+    "high": "",
+    "hot": "meni",
+    "kind": "",
+    "large": "",
+    "little": "",
+    "new": "",
+    "old": "",
+    "round": "",
+    "small": "",
+
+    //ADVERBS
+    "even": "",
+    "such": "",
+    "well": "",
+    "again": "",
+    "only": "",
+
+    //VERBS
+    "act": "",
+    "add": "",
+    "are": "",
+    "ask": "",
+    "be": "",
+    "build": "", 
+    "can": "",
+    "change": "",
+    "do": "",
+    "does": "",
+    "follow": "",
+    "get": "",
+    "had": "",
+    "have": "",
+    "is": "",
+    "live": "",
+    "make": "",
+    "must": "",
+    "need": "",
+    "place": "",
+    "play": "",
+    "put": "",
+    "read": "",
+    "said": "",
+    "set": "",
+    "take": "",
+    "tell": "",
+    "try": "",
+    "want": "",
+    "was": "",
+    "went": "",
+    "were": "",
+    "will": "",
+    "work": "",
+
+    //NOUNS
+    "": "",
+    "air": "",
+    "animal": "",
+    "earth": "",
+    "father": "",
+    "hand": "",
+    "home": "",
+    "house": "",
+    "light": "",
+    "man": "",
+      "men": "",
+    "mother": "",
+    "picture": "",
+    "port": "",
+    "self": "",
+    "time": "",
+    "word": "daen",
+    "world": "",
+    "woman": "",
+      "women": "",
+    "part": "",
+    "year": "",
+
+    //MULTICLASS
+    "as": "oli", //adverb, conjunction, preposition
+    "by": "caer", //adverb, preposition
+    "each": "", //adjective, adverb, noun
+    "end": "", //noun, verb
+    "for": "sen", //conjunction, preposition
+    "here": "", //adjective, adverb, noun
+    "how": "", //adjective, adverb, conjunction, noun
+    "other": "", //adjective, adverb, noun, pronoun
+    "some": "kerran", //adverb, pronoun
+    "what": "yulin", //adjective, adverb, interjection, pronoun
+    "which": "", //adjective, pronoun
+    "spell": "",
+    "land": "", //noun, verb
+    "why": "",  //adjective, adverb, pronoun
+    "off": "",  //adjective, adverb, preposition
+    "point": "", //noun, verb
+    "any": "", //adverb, pronoun
+    "where": "", //adverb, conjunction, noun, pronoun
+    "after": "", //adverb, conjunction, preposition
+  };
